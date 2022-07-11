648b4870f674daf2ee0de20f47aa32506c840886
jaykakadiya18 <jaykakadiya2014@gmail.com>
2022-07-11 22:10:45 +0530

    push on air

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="127.0.0.1", port=8000, reload=True)
+    # web: uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}
+    uvicorn.run("server.app:app", host="0.0.0.0", port=5000, reload=True)
