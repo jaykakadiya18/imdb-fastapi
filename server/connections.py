dfb9e76612df3a3aa438293ab6b493136d4a4078
Jay Kakadiya <jaykakadiya2014@gmail.com>
2022-07-12 11:44:07 +0530

    remove mongo link

--- server/connections.py
+++ server/connections.py
@@ -1,7 +1,7 @@
 import motor.motor_asyncio
 from bson.objectid import ObjectId
 
-MONGO_DETAILS = "mongodb+srv://jaykakadiya:jaykakadiya@cluster0.ddma8.mongodb.net/test"
+MONGO_DETAILS = ""
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 database = client.data
 
