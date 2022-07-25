f8a2a014410ca668eb7ce0d492d3a0749e60d480
Jay Kakadiya <jaykakadiya2014@gmail.com>
2022-07-25 10:58:03 +0530

    mongo sever removed

--- server/connections.py
+++ server/connections.py
@@ -1,7 +1,7 @@
 import motor.motor_asyncio
 from bson.objectid import ObjectId
 
-MONGO_DETAILS = "" #add mongodb server
+MONGO_DETAILS = "" #add mongodb server 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 database = client.data
 
